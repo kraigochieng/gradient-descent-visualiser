f93e8b5449f28ad49dcb1e96dc35b87226eb32d8
kraigochieng <kraigochieng@gmail.com>
2025-10-13 18:03:10 +0300

    added api base to runtime config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: false },
+	runtimeConfig: {
+		public: {
+			apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
+		}
+	},
 	css: ["~/assets/css/main.css"],
 	modules: ["@nuxt/ui"],
 	vite: {

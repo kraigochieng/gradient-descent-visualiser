96eee70464b38df02756b98f67a42a63d09bfd93
kraigochieng <kraigochieng@gmail.com>
2025-10-14 15:16:31 +0300

    added nuxt ui app wraper and global styling

--- client/app/app.vue
+++ client/app/app.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+useHead({
+	meta: [
+		{
+			name: "viewport",
+			content: "width=device-width, initial-scale=1.0",
+		},
+	],
+});
+</script>
+
 <template>
-	<NuxtPage />
+	<UApp>
+		<div class="page-wrapper">
+			<NuxtPage />
+		</div>
+	</UApp>
 </template>
+
+<style scoped>
+@reference "assets/css/main.css";
+
+* {
+	font-family: "Inter";
+}
+
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.page-wrapper {
+	@apply px-8 lg:px-32 py-8;
+}
+</style>

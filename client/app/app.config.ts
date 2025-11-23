b1cb7af1c1b090ab205085973d4b17b04701dc90
kraigochieng <kraigochieng@gmail.com>
2025-11-23 17:55:25 +0300

    added default variant for form field component

---
+++ client/app/app.config.ts
@@ -0,0 +1,14 @@
+// app.config.ts
+export default defineAppConfig({
+	ui: {
+		colors: {
+			primary: "palette-navy-blue",
+            secondary: "palette-red"
+		},
+        formField: {
+            defaultVariants: {
+                size: "xl"
+            }
+        }
+	},
+});
